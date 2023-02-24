f361585244c37c3504e2dfc76735d043dec4cbcd
dev-olanma <olanmaorji13@gmail.com>
2023-02-23 16:35:38 -0800

    A function that checks for uppercase character

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,15 +1,16 @@
 #include "main.h"
 
 /**
-* _isupper - checks for uppercase character
-* @c: Variable text
-* Return: Always 0.
-*/
+ * _isupper - Entry point
+ * Description: Checks for uppercase character
+ * @c: Integer variable
+ * Return: 1 if uppercase, 0 if not
+ */
 
 int _isupper(int c)
 {
-if (c >= 'A' && c <= 'Z')
-return (1);
-else
-return (0);
+	if (c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
 }
