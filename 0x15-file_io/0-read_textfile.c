5b4370f8d4c8e5da36283cfb2b07aabf5e27d45c
dev-olanma <olanmaorji13@gmail.com>
2023-04-10 11:36:44 -0700

    a function that reads a text file and prints it to the POSIX standard output.

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -14,31 +14,31 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	/* declare variables */
-	ssize_t open_file, read_file, write_file;
-	/* buffer to store the text */
-	char *buffer;
-       
-	/* check if filename is NULL and return 0 */
+	char *buffer; /* Buffer to store text */
+	ssize_t fd, my_read, my_write;
+	/* File descriptor, number of bytes read or written */
+
+	/* Check if filename is NULL */
 	if (!filename)
 		return (0);
-        /* give memory to buffer */
+	/* Allocate memory for the buffer */
 	buffer = malloc(sizeof(char) * letters);
 	if (!buffer)
 		return (0);
-
-	open_file = open(filename, O_RDONLY);
-	read_file = read(open_file, buffer, letters);
-	write_file = write(STDOUT_FILENO, buffer, read_file);
-        /* do some checks */
-	if (open_file == -1 || read_file == -1 || write_file != read_file)
+	/* Open the file to be read and store fd*/
+	fd = open(filename, O_RDONLY);
+	/* Read from the file and stores number of bytes read */
+	my_read = read(fd, buffer, letters);
+	/* Write to standard output and store the number of bytes written */
+	my_write = write(STDOUT_FILENO, buffer, my_read);
+	if (fd == -1 || my_read == -1 || my_write != my_read)
 	{
 		free(buffer);
 		return (0);
 	}
-        /* free memory and close open_file */
+	/* Free the memory and close the file descriptor (fd) */
 	free(buffer);
-	close(open_file);
+	close(fd);
 
-	return (write_file);
+	return (my_write);
 }
