8805405332ad59c7e2ebb552f2fcac0520b254f6
dev-olanma <olanmaorji13@gmail.com>
2023-04-10 12:55:52 -0700

    modify 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -13,11 +13,11 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int o, w, len = 0; /*declare variables */
-        /*check if filenmae is NULL */
+	int o, w, len = 0;
+
 	if (filename == NULL)
 		return (-1);
-        /* check if text_content is not NULL, loop through it and increment len starting from 0 */
+
 	if (text_content != NULL)
 	{
 		for (len = 0; text_content[len];)
@@ -26,10 +26,10 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	o = open(filename, O_WRONLY | O_APPEND);
 	w = write(o, text_content, len);
-        /* do some checks */
+
 	if (o == -1 || w == -1)
 		return (-1);
-        /* close file */
+
 	close(o);
 
 	return (1);
